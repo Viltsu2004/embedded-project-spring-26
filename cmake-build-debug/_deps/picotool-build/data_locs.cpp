0f176b36a5c496f1ab343abd0b0eef511b3bdb31
LTaskinen <lauri.taskinen2@metropolia.fi>
2026-05-14 22:22:06 +0300

    Taikanumeroita pois

---
+++ cmake-build-debug/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","C:/Users/litas/CLionProjects/embedded-project-spring-26/cmake-build-debug/_deps/picotool/"
+};
